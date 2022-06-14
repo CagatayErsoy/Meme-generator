672940256b83cc1e1f3b5ed984044715570238e5
Latecoder-hub <cagatay.ersoy1@gmail.com>
2022-06-14 13:40:14 -0700

    almost done

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,37 +3,68 @@ import React from 'react';
 import Data from './Data'  
 
 
-function Meme() {
-  const [meme,setMeme]=React.useState({
-    topText: "",
-    bottomText:"",
-    randomImage:""
-  })
- 
+export default function Meme() {
 
-  let url
-  const memesImage=Data.data.memes
-  function getrandomImage(e){
-    e.preventDefault(); 
-  const random=memesImage[Math.floor(Math.random()*memesImage.length)]
-  url=random.url
-  setMeme(prevState=> ({
-    ...prevState,
-    randomImage:url
-  }))
   
+  const [meme, setMeme] = React.useState({
+      topText: "",
+      bottomText: "",
+      randomImage: "http://i.imgflip.com/1bij.jpg" 
+  })
+  const [allMemeImages, setAllMemeImages] = React.useState(Data)
+  function handleChance(event){
+      const {name,value}=event.target
+      setMeme(prevMeme=>({
+          ...prevMeme,
+          [name]: value
+          
+      }))
+  }
+  
+  function getMemeImage() {
+      const memesArray = allMemeImages.data.memes
+      
+      const randomNumber = Math.floor(Math.random() * memesArray.length)
+      const url = memesArray[randomNumber].url
+      setMeme(prevMeme => ({
+          ...prevMeme,
+          randomImage: url
+      }))
+      
+  }
+  
+  return (
+      <main>
+          <div className="form">
+             <div className="text-boxes">    
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    onChange={handleChance}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                      onChange={handleChance}
+                />
+              </div> 
+         
+              <button 
+                  className="button"
+                  onClick={getMemeImage}
+              >
+                  Get a new meme image 🖼
+              </button>
+          </div>
+          <div className="meme">
+              <img src={meme.randomImage} className="memeImage" alt="memeImage" />
+              <h2 className="meme--text top">{meme.topText}</h2>
+              <h2 className="meme--text bottom">{meme.bottomText}</h2>
+          </div>
+      </main>
+  )
 }
-  return (<div className='wrapper'>
-    <form className='meme'>
-        <div className='form'>
-       <input type="text" placeholder='Shut Up'/>
-       <input type="text" placeholder='take my money'/>
-       </div>
-       <button  className="button" onClick={getrandomImage}> Get a new meme image &#128444;</button>
-    </form>
-    <img src={meme.randomImage} alt="meme" className='memeImage'/>
-    </div>
-  );
-}
-
-export default Meme;
